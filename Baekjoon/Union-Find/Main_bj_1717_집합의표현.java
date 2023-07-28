49c531080f8b03905de26325ac76e125fe969bf2
tjrcks2237 <tjrcks2237@naver.com>
2023-07-28 19:18:56 +0900

    23/07/28 백준_집합의표현(1717)

---
+++ Baekjoon/Union-Find/Main_bj_1717_집합의표현.java
@@ -0,0 +1,64 @@
+package d2307;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_1717_집합의표현 {
+
+    static int parent[];
+
+    public static void main(String[] args) throws Exception {
+
+        System.setIn(new FileInputStream("res/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int type = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (type == 0) {
+                union(a, b);
+            } else if (type == 1) {
+
+                a = find(a);
+                b = find(b);
+
+                if (a == b) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+        }
+    }
+
+    static int find(int x) {
+        if (x == parent[x]) return x;
+        else return parent[x] = find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) {
+            if (x < y) {
+                parent[y] = x;
+            } else {
+                parent[x] = y;
+            }
+        }
+    }
+}
